57a5a01aa3938803f90b417eaad422ab77ca7027
Sejmou <samo.kolter@gmail.com>
2022-12-10 13:01:45 +0100

    add script for extracting top 3 artists and list of genres built from their genres for each track

--- data_exploration_and_crawling/extract_track_artists_and_genres.py
+++ data_exploration_and_crawling/extract_track_artists_and_genres.py
@@ -1,3 +1,5 @@
+# This script extracts the first 3 featured artists (name + ID, 2 * 3 columns - note: I picked the top 3 as only around 5% of the songs feature more than 3 artists)
+# for each track in tracks.csv and uses their associated genres (contained in artist_data.csv) to obtain a list of genre tags for each song, stored in yet another column
 # %%
 import pandas as pd
 from helpers import get_data_path, create_data_out_path
@@ -16,16 +18,22 @@ artist_data = pd.read_csv(
     converters={"genres": literal_eval},
 )
 # %%
-def get_artist_data(artist_ids):
-    return artist_data.loc[artist_ids]
+def extract_track_artists_and_genres(track, max_artists=3):
+    artists = get_artist_names_and_ids_df(track.artists[:max_artists])
+    # unfortunately, Spotify tracks don't have song-level genre labels
+    # the next best thing is using the genres of the artists that feature in the song
+    track_genres = get_unique_genres(artists.index)
+    artists = artists.reset_index().set_index("rank")
+    res = {"track_id": track["id"]}
+    for rank, artist in artists.iterrows():
+        rank = str(rank)
+        res["artist_id_" + rank] = artist.id
+        res["artist_name_" + rank] = artist["name"]
+    res["genres"] = track_genres
+    return res
 
 
-def get_artist_genres(artist_ids):
-    data = get_artist_data(artist_ids)
-    return data.genres
-
-
-def get_artist_names_and_ids(artist_objs):
+def get_artist_names_and_ids_df(artist_objs):
     relevant_artist_fields = ["id", "name"]
     artists = pd.DataFrame(
         [
@@ -38,39 +46,36 @@ def get_artist_names_and_ids(artist_objs):
     return artists
 
 
-def get_genres(artists):
-    # therefore our goal is to create a genre list
-    # where genres of primary artist take highest precendence, followed by those of second, ...
-    genres = get_artist_genres(artists.index)
-    artist_genres = []  # may include duplicates if several artists with same genre
-    for cur_genres in genres:
-        artist_genres.extend(cur_genres)
+def get_unique_genres(artist_ids):
+    # our goal is to create a genre list where genres of primary artist take highest precendence,
+    # followed by those of the second, third ...
+    genres_per_artist = get_genres_per_artist(artist_ids)
+    genres = (
+        []
+    )  # may include duplicates at this point if several artists have same genre
+    for artist_genres in genres_per_artist:
+        genres.extend(artist_genres)
     # keep only first occurence of each genre
     #  - for details on how that works see https://stackoverflow.com/a/53657523/13727176
-    return list(dict.fromkeys(artist_genres))
+    return list(dict.fromkeys(genres))
 
 
-def extract_artists_and_genres(track, max_artists=5):
-    artists = get_artist_names_and_ids(track.artists[:max_artists])
-    # unfortunately, Spotify tracks don't have song-level genre labels
-    # the next best thing is using the genres of the artists that feature in the song
-    track_genres = get_genres(artists)
-    artists = artists.reset_index().set_index("rank")
-    res = {"track_id": track["id"]}
-    for rank, artist in artists.iterrows():
-        rank = str(rank)
-        res["artist_id_" + rank] = artist.id
-        res["artist_name_" + rank] = artist["name"]
-    res["genres"] = track_genres
-    return res
+def get_genres_per_artist(artist_ids):
+    data = get_artist_data(artist_ids)
+    return data.genres
 
 
-track_artists_and_genres = tracks.apply(
-    extract_artists_and_genres, axis=1, result_type="expand"
-)
+def get_artist_data(artist_ids):
+    return artist_data.loc[artist_ids]
 
 
 # %%
-track_artists_and_genres.to_csv(
-    create_data_out_path("track_artists_and_genres.csv"), index=False
-)
+track_artists_and_genres = tracks.apply(
+    extract_track_artists_and_genres, axis=1, result_type="expand"
+).set_index("track_id")
+# sort columns alphabetically
+track_artists_and_genres = track_artists_and_genres.sort_index(axis=1)
+
+
+# %%
+track_artists_and_genres.to_csv(create_data_out_path("track_artists_and_genres.csv"))
